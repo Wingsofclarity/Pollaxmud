8d3a478c3b19205bca1630297097e281a5ba5432
A. Bergqvist <amandarina@telia.com>
2015-12-09 13:52:09 +0100

    Trivialt.

--- TestPlayer.java
+++ TestPlayer.java
@@ -1,18 +1,25 @@
-
 import static org.junit.Assert.*;
 import org.junit.Test;
+import java.util.*;
 
-public class TestPlayer extends TestCase {
-  @Test
-  private Player player;
+public class TestPlayer /*extends TestCase*/ {
+    private Player player;
     private int negativeHp;
     private Room room;
+    private Course course;
 
+ @Test
   public void setUp(){
       this.room = new Room("Testrum");
-      this.player = new Player("Testspelare",this.room, new LinkedList<Course>() )
+      this.course = new Course("1", "Testkurs", 50);
+      LinkedList<Course> courses = new LinkedList<>();
+      courses.add(course);
+      this.player = new Player("Testspelare",this.room, courses);
   }
 
+
+ @Test
   public void testCreatePlayer() {
       assertEquals("Player does not get correct hp.", 60, this.player.getHp());
   }
+}
